24e1b28db4ed62cc3134373b3b05c76f0389136a
Matthew Guthrie <gunther@Matthews-MacBook-Air.local>
2023-04-26 14:43:30 -0400

    Update utils

--- chess_utils.py
+++ chess_utils.py
@@ -1,2 +1,11 @@
+import os
+
 def game_over(board):
     return board.is_game_over(claim_draw=True)
+
+if os.name == "posix":
+    STOCKFISH_PATH = "/opt/homebrew/opt/stockfish/bin/stockfish"
+else:
+    STOCKFISH_PATH = None
+
+DEFAULT_DEPTH = 3
